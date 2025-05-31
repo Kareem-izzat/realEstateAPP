ec0d7fa5beffe979fdac2e278937d462fad2e5e0
Kareem-izzat <97916953+Kareem-izzat@users.noreply.github.com>
2025-06-01 01:34:18 +0300

    shared prefrences

--- app/src/main/java/com/example/realestate/activites/LoginActivity.java
+++ app/src/main/java/com/example/realestate/activites/LoginActivity.java
@@ -2,6 +2,9 @@ package com.example.realestate.activites;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,9 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.realestate.R;
+import com.example.realestate.utils.SharedPrefManager;
 
 public class LoginActivity extends AppCompatActivity {
-
+    SharedPrefManager sharedPrefManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,5 +33,19 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
             startActivity(intent);
         });
+        CheckBox cbRememberMe = findViewById(R.id.cbRememberMe);
+        EditText etEmail = findViewById(R.id.etEmail);
+        String savedEmail = SharedPrefManager.getInstance(this).readString("email", "");
+        etEmail.setText(savedEmail);
+        sharedPrefManager =SharedPrefManager.getInstance(this);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(v -> {
+            if (cbRememberMe.isChecked()) {
+                sharedPrefManager.getInstance(this).writeString("email", etEmail.getText().toString());
+            } else {
+                sharedPrefManager.getInstance(this).writeString("email", "");
+            }
+        });
+
     }
 }
